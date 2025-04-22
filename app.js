eb7d696521098294826b1b5e402c9d1f57d5c70b
unknown <joovictor999@gmail.com>
2025-04-22 11:11:09 -0300

    criado arquivo referente a parte do javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+function enviarWhatsapp(event) { 
+    event.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5549998369021';
+
+    const texto = `Olá, Me chamo ${nome}, ${mensagem}`;
+    const msgFormatada = encodeURIComponent(texto);
+
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+}
